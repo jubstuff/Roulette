a0792e1581e2eb924d6bca2e8d0bf00f0bf31256
Giustino Borzacchiello <giustinob@gmail.com>
2010-11-05 22:30:49 +0100

    Aggiunto makefile - Aggiunto .gitignore - Aggiunta gestione errori per alcune funzioni

--- estrazione.c
+++ estrazione.c
@@ -1,12 +1,11 @@
 /*estrazione.c
- * =COMPILAZIONE=
+ * =COMPILAZIONE= FIXME
  * gcc -Wall -l pthread -o server-roulette estrazione.c common_header.h common_header.c
  *
  * TODO inserire descrizione file
  */
 
 #include "common_header.h"
-
 
 //TODO inserire descrizioni e nomi significativi per le variabili globali
 pthread_mutex_t sem = PTHREAD_MUTEX_INITIALIZER;
@@ -21,32 +20,32 @@ int estratto = -1;
  *
  *============================================================================*/
 void *croupier(void *arg) {
-	struct timespec cond_time;//c'è solo cond_time in questa struct
-	time_t now;//Conta i secondi da gennaio 1970 alle ore 00:00:00
+	struct timespec cond_time; //c'è solo cond_time in questa struct
+	time_t now; //Conta i secondi da gennaio 1970 alle ore 00:00:00
 	int status;
-	int intervallo = (int)arg;
-	
+	int intervallo = (int) arg;
+
 	//inizializzo il seme per la generazione di numeri random
 	srand(time(NULL));
-	
-	while(1) {
+
+	while (1) {
 		//Blocco il mutex per il croupier che fa l'estrazione
 		pthread_mutex_lock(&sem); //TODO inserire gestione errori
-		
+
 		//estrazione del numero 
 		estratto = rand() % 37;
-		
+
 		printf("CROUPIER estratto=%d\n", estratto);
 		/*risveglio i giocatori*/
-		pthread_cond_broadcast(&cond); //TODO inserire gestione errori
-		
+		pthread_cond_broadcast(&cond); /*TODO inserire gestione errori*/
+
 		now = time(NULL);
 		cond_time.tv_sec = now + intervallo;
 		cond_time.tv_nsec = 0;
 		//Attende che la condizione sia true in un tempo specificato da cond_time
 		status = pthread_cond_timedwait(&croupier_cond, &sem, &cond_time); //TODO inserire gestione errori
 		//Se status == ETIMEDOUT, significa che è scaduto il tempo senza la verifica della condizione
-		if( status == ETIMEDOUT) {
+		if (status == ETIMEDOUT) {
 			//Il croupier chiude le puntate
 			printf("CROUPIER tempo scaduto!!! chiudo le puntate\n");
 			estratto = -1;
@@ -59,20 +58,25 @@ void *croupier(void *arg) {
 	pthread_exit(NULL);
 }
 //TODO inserire descrizione funzione
+
 /**
  * FUNZIONE player
  *
  *============================================================================*/
 void *player(void *arg) {
-	int num = (int)arg;
+	int num = (int) arg;
 	int letto = 0;
-	while(1) {
+	int status;
+	while (1) {
 		//Il player prende il possesso del mutex
-		pthread_mutex_lock(&sem); //TODO inserire gestione errori
-		
+		status = pthread_mutex_lock(&sem);
+		if( status != 0 ) {
+			err_abort(status, "Lock sul mutex nel player")
+		}
+
 		/* in realtà la condizione (estratto < 0) va intesa come
 		 * (puntate_aperte == 1) */
-		while( (estratto < 0) || (letto == 1) ) {/*se le puntate sono aperte*/
+		while ((estratto < 0) || (letto == 1)) {/*se le puntate sono aperte*/
 			printf("GIOCATORE %d CONDIZIONE FALSA\n", num);
 			letto = 0;
 			pthread_cond_wait(&cond, &sem); //TODO inserire gestione errori
@@ -80,11 +84,11 @@ void *player(void *arg) {
 		//Gestire il caso del rilascio del mutex alla puntata 
 		/* arrivati a questo punto le puntate sono aperte, quindi i 
 		 * player possono accettare le puntate da parte dei client*/
-		 //io scriverei così
-                 //if((puntate_aperte == 0)){
-                   //il player deve fermarsi 
-                 //}
-                 //il player accetta puntate dal client
+		//io scriverei così
+		//if((puntate_aperte == 0)){
+		//il player deve fermarsi
+		//}
+		//il player accetta puntate dal client
 		printf("GIOCATORE %d leggo il numero estratto=%d\n", num, estratto);
 		//leggo il numero estratto
 		letto = 1;
@@ -94,20 +98,19 @@ void *player(void *arg) {
 	pthread_exit(NULL);
 }
 
-
 int main(int argc, char **argv) {
 	/* controllo numero di argomenti */
-	if(argc != 3) {
+	if (argc != 3) {
 		printf("Utilizzo: %s <numero porta> <intervallo secondi>\n", argv[0]);
 		exit(1);
 	} //TODO controllo errori più robusto
-	
-	int sockfd, clientfd; 					/* socket e client descriptor */
-	short int server_port;					/* porta del server */
-	int game_interval;						/* durata possibilità puntate */
-	struct sockaddr_in self, client_addr;	/* info del server e client */
-	socklen_t client_len = sizeof(client_addr);
-	int status; 							/* raccoglie i valori restituiti 
+
+	int sockfd, clientfd; /* socket e client descriptor */
+	short int server_port; /* porta del server */
+	int game_interval; /* durata possibilità puntate */
+	struct sockaddr_in self, client_addr; /* info del server e client */
+	socklen_t client_len = sizeof (client_addr);
+	int status; /* raccoglie i valori restituiti
 											dalle system call */
 	pthread_t thread, croupier_tid;
 	client_t *client_info;
@@ -116,51 +119,51 @@ int main(int argc, char **argv) {
 	server_port = atoi(argv[1]);
 	game_interval = atoi(argv[2]);
 	printf("La giocata durerà %d secondi\n", game_interval);
-	
+
 	/* creo il thread CROUPIER */
-	status = pthread_create(&croupier_tid, NULL, croupier, (void *)game_interval);
+	status = pthread_create(&croupier_tid, NULL, croupier, (void *) game_interval);
 	if (status != 0) {
 		err_abort(status, "Creazione del thread croupier");
 	}
-	
+
 	/* apro il socket */
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if( sockfd < 0 ) {
+	if (sockfd < 0) {
 		err_abort(errno, "Creazione socket");
 	}
-	
+
 	/* preparo la struct con le informazioni del server */
-	bzero(&self, sizeof(self));
+	bzero(&self, sizeof (self));
 	/* protocol family in host order */
 	self.sin_family = AF_INET;
 	self.sin_port = htons(server_port);
 	self.sin_addr.s_addr = htonl(INADDR_ANY);
-	
+
 	/* collego il socket */
-	status = bind(sockfd, (struct sockaddr *)&self, sizeof(self));
-	if( status != 0 ) {
+	status = bind(sockfd, (struct sockaddr *) &self, sizeof (self));
+	if (status != 0) {
 		err_abort(errno, "Bind socket");
 	}
 	/* pone il server in ascolto */
 	status = listen(sockfd, 5);
-	if( status != 0 ) {
+	if (status != 0) {
 		err_abort(errno, "Error in listening to socket");
 	}
-	
-	while(1) {
+
+	while (1) {
 		/* accetta connessioni dai client */
-		clientfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_len);
-		if (clientfd < 0 ) {
+		clientfd = accept(sockfd, (struct sockaddr *) &client_addr, &client_len);
+		if (clientfd < 0) {
 			err_abort(errno, "Error accepting connection");
 		}
 		/* inserisco informazioni sul client da inviare al thread player */
 		client_info = NULL;
-		client_info = malloc(sizeof(client_t));
+		client_info = malloc(sizeof (client_t));
 		client_info->client_data = client_addr;
 		client_info->clientfd = clientfd;
-		
-		status = pthread_create(&thread, NULL, player, (void *)client_info);
-		if( status != 0 ) {
+
+		status = pthread_create(&thread, NULL, player, (void *) client_info);
+		if (status != 0) {
 			err_abort(status, "Creazione thread");
 		}
 	}
