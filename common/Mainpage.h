6a0f6ccc12fe8f268a228e093a9b1c6fc99bda89
Giustino Borzacchiello <giustinob@gmail.com>
2011-02-11 21:39:29 +0100

    Seconda bozza documentazione

---
+++ common/Mainpage.h
@@ -0,0 +1,9 @@
+/**
+ * @mainpage Roulette
+ *
+ * @authors Giustino Borzacchiello, Antonio Cifariello, Francesco Paolo Cimmino
+ *
+ * @section intro Introduzione
+ *
+ * Questa è la pagina iniziale del progetto Roulette
+ */
